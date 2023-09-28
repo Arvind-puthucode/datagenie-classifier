ed4875bf23e6f254a413f48bdf01175455f6cd65
Arvind Puthucode <123arvindputhucode@gmail.com>
2023-09-28 15:49:23 +0530

    parameter+done

---
+++ src/manual_params.py
@@ -0,0 +1,122 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.stattools import adfuller, acf
+from statsmodels.stats.diagnostic import het_arch
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.stats.stattools import jarque_bera
+import statsmodels.api as sm
+from scipy.stats import skew, kurtosis
+import pandas as pd
+class ParametersManual:
+    def __init__(self, data_set):
+        self.df = data_set
+
+    def measure_trend(self, timeseries):
+        # Fit a linear regression model to the time series
+        x = np.arange(len(timeseries)).reshape(-1, 1)
+        model = LinearRegression()
+        model.fit(x, timeseries)
+        return model.coef_[0]
+    def measure_seasonality(self, timeseries):
+        # Calculate the power spectrum
+        fft_values = np.fft.fft(timeseries)
+        power_spectrum = np.abs(fft_values) ** 2
+
+        # Identify the dominant frequency
+        max_freq_index = np.argmax(power_spectrum[1:len(timeseries) // 2]) + 1
+        dominant_freq = max_freq_index / len(timeseries)
+
+        # Calculate the ratio of power at the dominant frequency to total power
+        power_ratio = np.max(power_spectrum) / np.sum(power_spectrum)
+        return power_ratio
+    def measure_autocorrelation(self, timeseries):
+        # Calculate autocorrelation
+        acf_values = acf(timeseries, nlags=10, fft=False)
+        max_autocorr = np.max(np.abs(acf_values))
+        return max_autocorr
+
+    def measure_stationarity(self, timeseries):
+        # Perform ADF test for stationarity
+        result = adfuller(timeseries)
+        return result[1] <= 0.05  # True if stationary, False if not
+
+    def measure_heteroscedasticity(self, timeseries):
+        # Perform ARCH test for heteroscedasticity
+        _, p_value, _, _ = het_arch(timeseries)
+        return p_value <= 0.05  # True if heteroscedastic, False if not
+
+    def measure_residual_patterns(self, timeseries):
+    # Fit an ARIMA model to detect residual patterns
+    # Using ARIMA(1, 0, 0) as an example order, you can adjust the order as needed
+        model = ARIMA(timeseries, order=(1, 0, 0))
+        model_fit = model.fit()
+        residuals = model_fit.resid
+        _, jb_p_value, _, _ = jarque_bera(residuals)
+        return jb_p_value < 0.05  # True if patterns in residuals (reject else)
+
+    def measure_outliers(self, timeseries,threshold=3):
+         # Calculate mean and standard deviation
+        mean_val = np.mean(timeseries)
+        std_dev = np.std(timeseries)
+
+        # Calculate Z-scores for each data point
+        z_scores = np.abs((timeseries - mean_val) / std_dev)
+
+        # Identify outliers based on the threshold
+        outliers = z_scores > threshold
+
+        # Count the number of outliers
+        num_outliers = np.sum(outliers)
+
+        return num_outliers
+
+    def measure_non_linearity(self, timeseries):
+        # Calculate skewness and kurtosis as indicators of non-linearity
+        skewness = skew(timeseries)
+        kurt = kurtosis(timeseries)
+        return 'Non-Linear' if abs(skewness) > 1 or abs(kurt) > 1 else 'Linear'
+
+    def measure_periodicity(self, timeseries, threshold=0.2):
+        # Calculate autocorrelation
+        acf_values = acf(timeseries, nlags=10, fft=False)
+
+        # Check if any autocorrelation exceeds the threshold
+        has_periodicity = np.any(acf_values > threshold)
+
+        return 'Yes' if has_periodicity else 'No'
+
+    def measure_magnitude_of_fluctuations(self, timeseries):
+        # Calculate standard deviation as a measure of fluctuations
+        return np.std(timeseries)
+
+    def measure_time_series_length(self, timeseries):
+        # Calculate the length of the time series
+        return len(timeseries)
+
+    def get_params(self):
+        self.df=self.df.set_index(self.df.columns[0])
+        #df = eg_df.fillna(df.mean())    
+        timeseries=self.df[self.df.columns[0]]
+        existing_params={
+            'Trend': self.measure_trend(timeseries),
+            'Seasonality': self.measure_seasonality(timeseries),
+            'Autocorrelation': self.measure_autocorrelation(timeseries),
+            'Stationarity': self.measure_stationarity(timeseries),
+            'Heteroscedasticity': self.measure_heteroscedasticity(timeseries),
+            'Residual Patterns': self.measure_residual_patterns(timeseries),
+            'Outliers': self.measure_outliers(timeseries),
+            'Non-Linearity': self.measure_non_linearity(timeseries),
+            'Periodicity': self.measure_periodicity(timeseries),
+            'Magnitude of Fluctuations': self.measure_magnitude_of_fluctuations(timeseries),
+            'Time Series Length': self.measure_time_series_length(timeseries)
+        }
+        return existing_params
+
+
+    # Add other manual parameter measurement functions here...
+
+if __name__ == "__main__":
+    eg_df = pd.read_csv("data/daily/sample_1.csv", index_col=0)
+    eg1_manual = ParametersManual(eg_df)
+    parameters_manual = eg1_manual.get_params(eg_df)
+    print(parameters_manual, "manual parameters")
--- src/parameter.py
+++ src/parameter.py
@@ -1,120 +1,37 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from statsmodels.tsa.stattools import adfuller, acf
-from statsmodels.stats.diagnostic import het_arch
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.stats.stattools import jarque_bera
-import statsmodels.api as sm
 import pandas as pd
-import pywt
-from scipy.stats import skew, kurtosis
-
+import tsfresh
+from manual_params import ParametersManual
 class Parameters:
     def __init__(self, data_set: pd.DataFrame):
         self.data = data_set
+    def measure_tsfresh_features(self, df):
+        # Extract relevant TSFresh features
+        tsfresh_features = tsfresh.extract_features(df,column_id="point_timestamp", column_sort="point_timestamp", column_value="point_value")
+        # Initialize a dictionary to hold TSFresh features
+        tsfresh_dict = {}
+        i=0
+        #print(tsfresh_features.columns,tsfresh_features.shape,tsfresh_features.iloc[:2,:].describe())
+        for feature_name in tsfresh_features.columns:
+            # Get the specified TSFresh feature (replace with actual feature names)
+            tsfresh_feature = tsfresh_features[feature_name].mean()
+            if not np.isnan(tsfresh_feature) and tsfresh_feature != 0:
+                tsfresh_dict[f'tsfeature{i+1}'] = tsfresh_feature
+            i+=1
+        return tsfresh_dict     
+    
+    def get_params(self, df):
+        tsfresh_features = self.measure_tsfresh_features(df)
+        new_params={}
+        manual_params=ParametersManual(data_set=df).get_params()
+        new_params.update(manual_params)
+        new_params.update(tsfresh_features)
+        return new_params
 
-    def measure_trend(self, timeseries):
-        # Fit a linear regression model to the time series
-        x = np.arange(len(timeseries)).reshape(-1, 1)
-        model = LinearRegression()
-        model.fit(x, timeseries)
-        return model.coef_[0]
-    def measure_seasonality(self, timeseries):
-        # Calculate the power spectrum
-        fft_values = np.fft.fft(timeseries)
-        power_spectrum = np.abs(fft_values) ** 2
-
-        # Identify the dominant frequency
-        max_freq_index = np.argmax(power_spectrum[1:len(timeseries) // 2]) + 1
-        dominant_freq = max_freq_index / len(timeseries)
-
-        # Calculate the ratio of power at the dominant frequency to total power
-        power_ratio = np.max(power_spectrum) / np.sum(power_spectrum)
-        return power_ratio
-    def measure_autocorrelation(self, timeseries):
-        # Calculate autocorrelation
-        acf_values = acf(timeseries, nlags=10, fft=False)
-        max_autocorr = np.max(np.abs(acf_values))
-        return max_autocorr
-
-    def measure_stationarity(self, timeseries):
-        # Perform ADF test for stationarity
-        result = adfuller(timeseries)
-        return result[1] <= 0.05  # True if stationary, False if not
-
-    def measure_heteroscedasticity(self, timeseries):
-        # Perform ARCH test for heteroscedasticity
-        _, p_value, _, _ = het_arch(timeseries)
-        return p_value <= 0.05  # True if heteroscedastic, False if not
-
-    def measure_residual_patterns(self, timeseries):
-    # Fit an ARIMA model to detect residual patterns
-    # Using ARIMA(1, 0, 0) as an example order, you can adjust the order as needed
-        model = ARIMA(timeseries, order=(1, 0, 0))
-        model_fit = model.fit()
-        residuals = model_fit.resid
-        _, jb_p_value, _, _ = jarque_bera(residuals)
-        return jb_p_value < 0.05  # True if patterns in residuals (reject else)
-
-    def measure_outliers(self, timeseries,threshold=3):
-         # Calculate mean and standard deviation
-        mean_val = np.mean(timeseries)
-        std_dev = np.std(timeseries)
-
-        # Calculate Z-scores for each data point
-        z_scores = np.abs((timeseries - mean_val) / std_dev)
-
-        # Identify outliers based on the threshold
-        outliers = z_scores > threshold
-
-        # Count the number of outliers
-        num_outliers = np.sum(outliers)
-
-        return num_outliers
-
-    def measure_non_linearity(self, timeseries):
-        # Calculate skewness and kurtosis as indicators of non-linearity
-        skewness = skew(timeseries)
-        kurt = kurtosis(timeseries)
-        return 'Non-Linear' if abs(skewness) > 1 or abs(kurt) > 1 else 'Linear'
-
-    def measure_periodicity(self, timeseries, threshold=0.2):
-        # Calculate autocorrelation
-        acf_values = acf(timeseries, nlags=10, fft=False)
-
-        # Check if any autocorrelation exceeds the threshold
-        has_periodicity = np.any(acf_values > threshold)
-
-        return 'Yes' if has_periodicity else 'No'
-
-    def measure_magnitude_of_fluctuations(self, timeseries):
-        # Calculate standard deviation as a measure of fluctuations
-        return np.std(timeseries)
-
-    def measure_time_series_length(self, timeseries):
-        # Calculate the length of the time series
-        return len(timeseries)
-
-    def get_params(self, timeseries):
-        return {
-            'Trend': self.measure_trend(timeseries),
-            'Seasonality': self.measure_seasonality(timeseries),
-            'Autocorrelation': self.measure_autocorrelation(timeseries),
-            'Stationarity': self.measure_stationarity(timeseries),
-            'Heteroscedasticity': self.measure_heteroscedasticity(timeseries),
-            'Residual Patterns': self.measure_residual_patterns(timeseries),
-            'Outliers': self.measure_outliers(timeseries),
-            'Non-Linearity': self.measure_non_linearity(timeseries),
-            'Periodicity': self.measure_periodicity(timeseries),
-            'Magnitude of Fluctuations': self.measure_magnitude_of_fluctuations(timeseries),
-            'Time Series Length': self.measure_time_series_length(timeseries)
-        }
 
 if __name__ == "__main__":
     eg_df = pd.read_csv("data/daily/sample_1.csv", index_col=0)
-    eg_df = eg_df.set_index(eg_df.columns[0])
-    eg_df = eg_df.fillna(eg_df.mean())
     eg1 = Parameters(eg_df)
-    timeseries = (eg1.data)[eg_df.columns[0]]
-    parameters = eg1.get_params(timeseries)
+    print(eg_df.head())
+    parameters = eg1.get_params(eg_df)
     print(parameters, "parameters")
