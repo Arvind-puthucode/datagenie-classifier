743ca7756f2f867c3dea97bef061212d37c95bfc
Arvind Puthucode <123arvindputhucode@gmail.com>
2023-10-15 17:32:24 +0530

    duplication

--- src/models/arima.py
+++ src/models/arima.py
@@ -5,19 +5,16 @@ from statsmodels.tsa.arima.model import ARIMA
 import itertools
 from joblib import Parallel, delayed
 
-class arimaModel:
+class ARIMAModel:
     def __init__(self, df: pd.DataFrame):
-        print(df.columns,df.index)
-        self.datearr=df.loc[:,'point_timestamp']
-        
-        df.drop(columns=[df.columns[0]], inplace=True)
-        df.index = pd.to_datetime(df.index)
-        self.data = df
+        self.datearr = df['point_timestamp']
+        self.data = df.drop(columns=[df.columns[0]])
+        self.data.index = pd.to_datetime(df.index)
         l = len(self.data)
         X = self.data.index
-        y = self.data[df.columns[0]]
+        y = self.data[self.data.columns[0]]
         limit = int(l * 0.7)
-        self.x_train, self.x_test, self.y_train, self.y_test = X[0:limit], X[limit:], y[0:limit], y[limit:]
+        self.x_train, self.x_test, self.y_train, self.y_test = X[:limit], X[limit:], y[:limit], y[limit:]
 
     def create_model(self):
         best_params = self.hyperparameter_optimization()
@@ -25,27 +22,21 @@ class arimaModel:
         model_fit = model.fit()
         y_pred = model_fit.forecast(steps=len(self.x_test))[0]
         print(f'The ARIMA model best fit order was {best_params}')
-        return self.mape(y_pred)
-    def result_json(self):
-        best_params = self.hyperparameter_optimization()
+        return self.result_json(y_pred)
 
-        model = ARIMA(self.y_train, order=best_params)
-        model_fit = model.fit()
+    def result_json(self, y_pred):
         y_pred_train = model_fit.predict(start=0, end=len(self.y_train) - 1)
         y_pred = model_fit.forecast(steps=len(self.x_test))
-        print(f'data',self.data)
+        print(f'data', self.data)
         print(f'The ARIMA model best fit order was {best_params}')
-        mape_err=self.mape(y_pred)
-        print(y_pred_train,y_pred)
-        l1,l2=y_pred_train.tolist(),y_pred.tolist()
+        mape_err = self.mape(y_pred)
+        print(y_pred_train, y_pred)
+        l1, l2 = y_pred_train.tolist(), y_pred.tolist()
         l1.extend(l2)
-        l3,l4=self.y_train.to_list(),self.y_test.tolist()
+        l3, l4 = self.y_train.to_list(), self.y_test.tolist()
         l3.extend(l4)
-        print(len(l1),len(l3),len(l2),len(l4))
-        return {"mape":mape_err,"point_timestamp":self.datearr.tolist()
-                ,"y_pred":l1,
-                "y_test":l3}
-    
+        print(len(l1), len(l3), len(l2), len(l4))
+        return {"mape": mape_err, "point_timestamp": self.datearr.tolist(), "y_pred": l1, "y_test": l3}
 
     def mape(self, y_pred):
         abs_diffs = np.abs(self.y_test - y_pred)
@@ -59,16 +50,12 @@ class arimaModel:
         d_range = range(0, 3)
         q_range = range(0, 3)
 
-        # Generate all possible combinations of p, d, and q
         all_params = list(itertools.product(p_range, d_range, q_range))
 
-        # Use parallel processing to find the best parameters
-        results = Parallel(n_jobs=-1)(
-            delayed(self._calculate_aic)(params) for params in all_params
-        )
+        results = Parallel(n_jobs=-1)(delayed(self._calculate_aic)(params) for params in all_params)
 
         best_aic = float("inf")
-        best_params = (0,0,0)
+        best_params = (0, 0, 0)
 
         for aic, params in results:
             if aic < best_aic:
@@ -90,6 +77,6 @@ class arimaModel:
 
 if __name__ == "__main__":
     eg_data = pd.read_csv("data/test/test_4.csv")
-    arima_model = arimaModel(eg_data)
+    arima_model = ARIMAModel(eg_data)
     best_params = arima_model.create_model()
     print(f"The best ARIMA model order is {best_params}")
--- src/models/lstm.py
+++ src/models/lstm.py
@@ -6,9 +6,9 @@ from keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 from keras.callbacks import EarlyStopping
 
-class lstmModel:
+class LSTMModel:
     def __init__(self, df: pd.DataFrame):
-        self.datearr=df.loc[:,'point_timestamp']
+        self.datearr = df.loc[:, 'point_timestamp']
         df.drop(columns=[df.columns[0]], inplace=True)
         df.index = pd.to_datetime(df.index)
         self.data = df
@@ -18,93 +18,59 @@ class lstmModel:
         limit = int(l * 0.7)
         self.x_train, self.x_test, self.y_train, self.y_test = X[0:limit], X[limit:], y[0:limit], y[limit:]
 
-    
     def create_model(self):
-        # Prepare the data
-        x_train, y_train = self.prepare_data(self.x_train, self.y_train)
-        x_test, y_test = self.prepare_data(self.x_test, self.y_test)
-
-        # Build the LSTM model
-        model = Sequential()
-        model.add(LSTM(units=100, activation='relu', input_shape=(x_train.shape[1], 1)))
-        model.add(Dropout(0.2))  # Dropout for regularization
-        model.add(Dense(units=1))
-        model.compile(optimizer='adam', loss='mean_squared_error')
-
-        # Early stopping to prevent overfitting
-        early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
-
-        # Train the model
-        model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stop])
-
-        # Predict using the trained model
-        y_pred = model.predict(x_test)
-        y_pred = self.inverse_transform(y_pred)
-
-        # Calculate MAPE
-        mape = self.mape(y_pred)
-
-        return mape
-
-    def result_json(self):
         # Prepare the data
         self.x_train, self.y_train = self.prepare_data(self.x_train, self.y_train)
         self.x_test, self.y_test = self.prepare_data(self.x_test, self.y_test)
 
-        # Build the LSTM model
+        # Build and train the LSTM model
+        model, y_pred, y_pred_train = self.train_lstm_model(self.x_train, self.y_train, self.x_test)
+
+        # Calculate MAPE
+        mape = self.mape(y_pred)
+
+        l1, l2 = y_pred_train.tolist(), y_pred.tolist()
+        l1.extend(l2)
+        l3, l4 = self.y_train.tolist(), self.y_test.tolist()
+        l3.extend(l4)
+
+        print(len(l1), len(l3), len(l2), len(l4))
+        return {"mape": mape, "point_timestamp": self.datearr.tolist(), "y_pred": l1, "y_test": l3}
+
+    def train_lstm_model(self, x_train, y_train, x_test):
         model = Sequential()
-        model.add(LSTM(units=100, activation='relu', input_shape=(self.x_train.shape[1], 1)))
-        model.add(Dropout(0.2))  # Dropout for regularization
+        model.add(LSTM(units=100, activation='relu', input_shape=(x_train.shape[1], 1)))
+        model.add(Dropout(0.2))
         model.add(Dense(units=1))
         model.compile(optimizer='adam', loss='mean_squared_error')
 
-        # Early stopping to prevent overfitting
         early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
+        model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, self.y_test), callbacks=[early_stop])
 
-        # Train the model
-        model.fit(self.x_train, self.y_train, epochs=50, batch_size=32, validation_data=(self.x_test, self.y_test), callbacks=[early_stop])
-
-        # Predict using the trained model
-        y_pred = model.predict(self.x_test)
+        y_pred = model.predict(x_test)
         y_pred = self.inverse_transform(y_pred)
 
-        # Predict using the trained model
-        y_pred_train = model.predict(self.x_train)
+        y_pred_train = model.predict(x_train)
         y_pred_train = self.inverse_transform(y_pred_train)
-        
-        # Calculate MAPE
-        mape = self.mape(y_pred)
-        l1,l2=y_pred_train.tolist(),y_pred.tolist()
-        l1.extend(l2)
-        l3,l4=self.y_train.tolist(),self.y_test.tolist()
-        l3.extend(l4)
-        print(len(l1),len(l3),len(l2),len(l4))
-        return {"mape":mape,"point_timestamp":self.datearr.tolist()
-                ,"y_pred":l1,
-                "y_test":l3}
-    
-    
+
+        return model, y_pred, y_pred_train
+
     def prepare_data(self, X, y, sequence_length=10):
-        # Normalize the data
         scaler = MinMaxScaler()
         y = np.array(y).reshape(-1, 1)
         y = scaler.fit_transform(y)
 
-        # Create sequences for LSTM
         x_seq, y_seq = [], []
         for i in range(len(X) - sequence_length):
-            x_seq.append(y[i:i+sequence_length, 0])
-            y_seq.append(y[i+sequence_length, 0])
+            x_seq.append(y[i:i + sequence_length, 0])
+            y_seq.append(y[i + sequence_length, 0])
 
         x_seq, y_seq = np.array(x_seq), np.array(y_seq)
-
-        # Reshape for LSTM
         x_seq = np.reshape(x_seq, (x_seq.shape[0], x_seq.shape[1], 1))
 
         return x_seq, y_seq
 
     def inverse_transform(self, y):
-        # Inverse transform the normalized values
         scaler = MinMaxScaler()
         scaler.fit_transform(self.y_test.reshape(-1, 1))
         y_inverse = scaler.inverse_transform(y)
@@ -112,15 +78,13 @@ class lstmModel:
 
     def mape(self, y_pred):
         self.y_test_no_zeros = np.where(self.y_test == 0, 1e-10, self.y_test)
-
         abs_diffs = np.abs(self.y_test_no_zeros - y_pred)
         pct_diffs = abs_diffs / self.y_test_no_zeros
         pct_diffs[np.isnan(pct_diffs)] = 0
         mape_error = np.mean(pct_diffs) * 100
         return mape_error
 
-
 if __name__ == "__main__":
     eg_data = pd.read_csv("data/daily/sample_1.csv", index_col="point_timestamp")
-    lstm_model = lstmModel(eg_data)
+    lstm_model = LSTMModel(eg_data)
     print(f'LSTM MAPE error is {lstm_model.create_model()}')
